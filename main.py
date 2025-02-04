b48dd05fb84819bc3c89061ba165a8a28a9c1f69
batista29 <natabatista2908@gmail.com>
2025-02-04 09:15:40 -0300

    Feat: Bot to stop following everbody on your instagram

---
+++ main.py
@@ -0,0 +1,29 @@
+import pyautogui
+import time
+
+# x always 1100
+# y always +73
+
+time.sleep(5)
+
+x = 1100
+y = 465
+
+following = 61 #put your number of following
+
+pyautogui.moveTo(1100, 465)
+
+while(following > 0):
+    for i in range(following):
+        
+        pyautogui.moveTo(1100, 465) #following button
+        time.sleep(0.3)
+        pyautogui.click() #click on the button
+
+        time.sleep(0.3)
+        pyautogui.moveTo(950, 655) #stop following button
+        pyautogui.click() #click on the button
+
+        time.sleep(0.3)
+        pyautogui.scroll(-72) #scroll the list
+        time.sleep(0.3)
---
+++ position.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+print(pyautogui.position())
+
+# Testes
+# pyautogui.moveTo(1100, 465)
+# pyautogui.scroll(-75)
